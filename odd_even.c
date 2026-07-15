61443044882412a4f2eae7de95160fc13711d66b
poonam370 <gadripoonam890@gmail.com>
2026-07-15 23:54:37 +0530

    Create odd_even.c

---
+++ odd_even.c
@@ -0,0 +1,22 @@
+//odd even number 
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0)
+    {
+        printf("Even Number");
+    }
+    else
+    {
+        printf("Odd Number");
+    }
+
+    return 0;
+}
