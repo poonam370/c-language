30a4896fccd2c398a24916b64eb44f80ffd237a6
poonam370 <gadripoonam890@gmail.com>
2026-07-16 23:59:57 +0530

    Create largest_no.c

---
+++ largest_no.c
@@ -0,0 +1,27 @@
+ //largest among three numbers 
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, largest;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a >= b && a >= c)
+    {
+        largest = a;
+    }
+    else if(b >= a && b >= c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
+
+    printf("Largest number = %d", largest);
+
+    return 0;
+}
