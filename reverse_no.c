19cc344b3f300f892eb6a9350c1286132b43727c
poonam370 <gadripoonam890@gmail.com>
2026-07-16 23:37:00 +0530

    Create reverse_no.c

---
+++ reverse_no.c
@@ -0,0 +1,20 @@
+// reverse number 
+#include <stdio.h>
+
+int main()
+{
+    int num, rev = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while(num != 0)
+    {
+        rev = rev * 10 + (num % 10);
+        num = num / 10;
+    }
+
+    printf("Reverse Number = %d", rev);
+
+    return 0;
+}
