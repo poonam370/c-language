2e136404148c564369ee6a155e0a47ad2b8fd2e0
poonam370 <gadripoonam890@gmail.com>
2026-07-16 23:53:20 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,19 +1,28 @@
+// calculate the factorial of a number using loop 
+
 #include <stdio.h>
 
 int main()
 {
     int num, i;
-    long long fact = 1;
+    long long int fact = 1;
 
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    for(i = 1; i <= num; i++)
+    if(num < 0)
     {
-        fact = fact * i;
+        printf("negative number is not possible.");
     }
+    else
+    {
+        for(i = 1; i <= num; i++)
+        {
+            fact = fact * i;
+        }
 
-    printf("Factorial = %lld", fact);
+        printf("Factorial of %d = %lld", num, fact);
+    }
 
     return 0;
 }
