76b9ab7f5143594c7dd8cd175c43ad1f59c59efd
poonam370 <gadripoonam890@gmail.com>
2026-07-16 23:20:49 +0530

    Update sumofdigit.c

--- sumofdigit.c
+++ sumofdigit.c
@@ -1,1 +1,20 @@
+//sum od the indivistual digit 
+#include <stdio.h>
 
+int main()
+{
+    int num, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while(num != 0)
+    {
+        sum = sum + (num % 10);
+        num = num / 10;
+    }
+
+    printf("Sum of digits = %d", sum);
+
+    return 0;
+}
